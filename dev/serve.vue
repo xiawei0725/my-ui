76b609e73fcfac2923783330d0e3e22506a82225
xiawei <54879490@qq.com>
2022-11-15 22:30:21 +0800

    init

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script>
+import Vue from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { ElementUiSample } from '@/entry.esm';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  // components: {
+  //  ElementUiSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <el-button></el-button>
+  </div>
+</template>
---
+++ src/lib/button/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <span class="my-button">btn</span>
+</template>
+
+<script>
+export default {
+    name: 'MyButton'
+}
+</script>
+
+<style scoped>
+.my-button {
+    padding:3px 8px;
+    background:green;
+    color:white;
+}
+</style>
